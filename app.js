ff7647a18b96a9447dfe8397a3cf1e6f1a795a65
Takuya Endo <endo@Takuyas-MacBook-Pro.local>
2017-03-16 21:18:43 +0900

    no message

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const fs = require('fs');
 const path = require('path');
 const API_GIFMAGAZINE_DOMAIN = 'http://api.gifmagazine.net';
 const API_GIFMAGAZINE_PATH = '/v1/gifs/search';
-const API_URL = API_GIFMAGAZINE_DOMAIN + API_GIFMAGAZINE_PATH;
+
+const API_URL = 'https://api.sportradar.us/soccer-t3/eu/en/schedules/2017-04-07/schedule.json?api_key=n387hsw9p7r8v59pkc85vkmy';
 const TEMPLATE = fs.readFileSync(path.join(__dirname, 'template', 'main.tmpl'));
 const compiled = _.template(TEMPLATE);
 
@@ -15,12 +16,30 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   const q = qs.stringify(req.query);
-  const url = API_URL + '?' + q;
-  axios.get(url).then((results) => {
-    const data = results.data.data;
-    const images = data.map((d) => `<li><img src=${d.image.default.url} /></li>`);
-    res.send(compiled({images: images, q: req.query.q}));
-  }).catch(next);
+  const url = API_URL;
+/*
+axios.get(url).then((results) => {
+	const data = results.sport_events;
+	const images = data.map((d) => `<li>${d.id}</li>`);
+	res.send(compiled({images: images}));
+}).catch(next);
+*/
+	axios.get(url)
+	  .then(function (results) {
+			const data = results.data.sport_events;
+			const games = data.map(function(x){
+
+				var teams = x.competitors.map(function(y){
+					return `<li>${y.name}</li>`;
+				});
+				return `<div><li>${x.scheduled}</li><li>${x.season.name}</li>` + teams + '</div>';
+			});
+	    console.log(games);
+			res.send(compiled({games: games}));
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
 });
 
 app.listen(PORT, () => {
