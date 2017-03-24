0ccba6218363ddf8f132a91fbb4e926faca52e3e
Takuya Endo <endo@Takuyas-MacBook-Pro.local>
2017-03-24 16:59:55 +0900

    bootflatを追加する

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const path = require('path');
 const API_GIFMAGAZINE_DOMAIN = 'http://api.gifmagazine.net';
 const API_GIFMAGAZINE_PATH = '/v1/gifs/search';
 
-const API_URL = 'https://api.sportradar.us/soccer-t3/eu/en/schedules/2017-04-07/schedule.json?api_key=n387hsw9p7r8v59pkc85vkmy';
+const API_URL = 'https://api.sportradar.us/soccer-t3/eu/en/schedules/2017-04-01/schedule.json?api_key=n387hsw9p7r8v59pkc85vkmy';
 const TEMPLATE = fs.readFileSync(path.join(__dirname, 'template', 'main.tmpl'));
 const compiled = _.template(TEMPLATE);
 
 const PORT = process.env.PORT || 3000;
+
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use((req, res, next) => {
   const q = qs.stringify(req.query);
@@ -30,9 +34,9 @@ axios.get(url).then((results) => {
 			const games = data.map(function(x){
 
 				var teams = x.competitors.map(function(y){
-					return `<li>${y.name}</li>`;
+					return `<ui>${y.name}</ui>`;
 				});
-				return `<div><li>${x.scheduled}</li><li>${x.season.name}</li>` + teams + '</div>';
+				return `<li class='list-group-item'><ui>${x.scheduled}</ui><ui>${x.season.name}</ui>` + teams + '</li>';
 			});
 	    console.log(games);
 			res.send(compiled({games: games}));
